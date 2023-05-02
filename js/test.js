23f790d2593d627b6cf84cec8fae387ec9e1dc53
Francisco Huerta <frhuerta.dwfsc16@ucamp.utel.edu.mx>
2023-05-01 21:45:40 -0600

    primer commit llamar Api

--- js/test.js
+++ js/test.js
@@ -1,1 +1,13 @@
-console.log("prueba")
+const llamarApiClima = async () => {
+  try {
+    const respuesta = await fetch(
+      "http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json"
+    );
+    const json = await respuesta.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+llamarApiClima();
